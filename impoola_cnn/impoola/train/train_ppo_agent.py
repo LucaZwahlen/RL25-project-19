fix: reward normalization issue (bandade fix, but might work)

impoola_cnn/impoola/train/train_ppo_agent.py
@@ -17,7 +17,7 @@ def train_ppo_agent(args, logger: Logger, envs, agent, optimizer, device):
     """ Train the PPO agent """
 
     # Track training episode statistics
-    episodeQueueCalculator = EpisodeQueueCalculator(True, 100, args.env_id, args.num_envs, args.distribution_mode, device)
+    episodeQueueCalculator = EpisodeQueueCalculator(True, args.normalize_reward, 100, args.env_id, args.num_envs, args.distribution_mode, device)
 
     # ALGO Logic: Storage setup
     obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape, device=device)

impoola_cnn/impoola/train/train_vtrace_agent.py
@@ -37,7 +37,7 @@ def train_vtrace_agent(args, logger: Logger, envs, agent, optimizer, device):
     learning_rate = optimizer.param_groups[0]["lr"].clone()
     max_grad_norm = torch.tensor(args.max_grad_norm, device=device)
 
-    episodeQueueCalculator = EpisodeQueueCalculator(True, 100, args.env_id, N, args.distribution_mode, device)
+    episodeQueueCalculator = EpisodeQueueCalculator(True, args.normalize_reward, 100, args.env_id, N, args.distribution_mode, device)
 
     global_step = 0
 

impoola_cnn/impoola/utils/csv_logging.py
@@ -13,12 +13,12 @@ from impoola_cnn.impoola.utils.environment_knowledge import (
 from impoola_cnn.impoola.utils.noop_indices import get_noop_indices
 from impoola_cnn.impoola.utils.success_rewards import get_success_reward
 
-EPSILON = 1e-7
+EPSILON = 0.25
 
 
 class EpisodeQueueCalculator:
-    def __init__(self, is_train, queue_len, env_id, num_envs, distribution_mode, device):
-
+    def __init__(self, is_train, normalize_reward, queue_len, env_id, num_envs, distribution_mode, device):
+        self.normalize_reward = normalize_reward
         self.optimal_path_length = try_get_optimal_train_path_length(env_id, distribution_mode) if is_train else try_get_optimal_test_path_length(env_id)
 
         self.ticks = torch.zeros((num_envs,), device=device)
@@ -48,10 +48,12 @@ class EpisodeQueueCalculator:
             self.noop_mask[idx] = 1
 
     def update(self, action, rewards):
+        # here we assume that when normalizing, the completed reward is large enough that it will go to the clamp max (10.0)
+        success_reward_fixed = torch.tensor(10.0) if self.normalize_reward else self.success_reward
         self.ticks += 1
         action_op_mask = ~(self.noop_mask[action])
         self.steps += action_op_mask
-        self.success = self.success | (torch.abs(rewards - self.success_reward) < EPSILON)
+        self.success = self.success | (rewards >= success_reward_fixed - EPSILON)
 
     def extend(self, info):
         completed_episodes = info["episode"]["r"][info["_episode"]]